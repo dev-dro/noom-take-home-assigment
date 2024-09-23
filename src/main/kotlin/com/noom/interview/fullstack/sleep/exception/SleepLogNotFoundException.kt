dd2578129b42b3cced0ea42bdcd1b559c6465dfe
dev-dro <danilo.ro@protonmail.com>
2024-09-22 22:13:47 -0300

    Create the SleepLogService.findSleepLogsAveragesFrom30Days function

--- src/main/kotlin/com/noom/interview/fullstack/sleep/exception/SleepLogNotFoundException.kt
+++ src/main/kotlin/com/noom/interview/fullstack/sleep/exception/SleepLogNotFoundException.kt
@@ -1,5 +1,5 @@
 package com.noom.interview.fullstack.sleep.exception
 
 class SleepLogNotFoundException : RuntimeException() {
-    override val message: String = "No sleep log found for last night"
+    override val message: String = "No sleep log found"
 }
