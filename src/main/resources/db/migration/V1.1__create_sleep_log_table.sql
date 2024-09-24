9dacc28cbd8d212e71b0224207864e0f46515c7a
dev-dro <danilo.ro@protonmail.com>
2024-09-24 20:54:12 -0300

    Create the enum type feeling in the database and change the morning_feeling column type to feeling

--- src/main/resources/db/migration/V1.1__create_sleep_log_table.sql
+++ src/main/resources/db/migration/V1.1__create_sleep_log_table.sql
@@ -1,8 +1,10 @@
+create type feeling as enum ('good', 'bad', 'ok');
+
 create table if not exists sleep_log (
     id int generated always as identity primary key,
     username varchar not null,
     log_date date not null,
     started_sleep_at timestamp not null,
     woke_up_at timestamp not null,
-    morning_feeling varchar(4) not null
+    morning_feeling feeling not null
 )
